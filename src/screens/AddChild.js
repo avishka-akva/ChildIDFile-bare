419db789f27dfaf6030852dfde03ec087d3bf89a
Avishka Shyaman <avishkas@akvasoft.com>
2022-11-22 17:00:37 +0530

    Added Progress Bar

--- src/screens/AddChild.js
+++ src/screens/AddChild.js
@@ -1,5 +1,6 @@
 import { StyleSheet, ScrollView, Text, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import * as Progress from "react-native-progress";
 
 import { globleStyles } from "../shared/style";
 
@@ -7,6 +8,16 @@ function AddChild() {
   return (
     <SafeAreaProvider style={styles.container}>
       <ScrollView style={{ width: "100%" }}>
+        <View style={styles.progressBar}>
+          <Progress.Bar
+            progress={0.3}
+            width={null}
+            height={9}
+            color={"#A352EB"}
+            borderColor={"#00000014"}
+            borderRadius={8}
+          />
+        </View>
         <View>
           <Text style={globleStyles.title}>Personal Information</Text>
         </View>
