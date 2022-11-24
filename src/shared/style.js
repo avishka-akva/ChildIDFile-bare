53a0611a82c95f0bd0a9d1a35b0b86b7b985f2a1
Avishka Shyaman <avishkas@akvasoft.com>
2022-11-24 16:38:53 +0530

    added button styles

--- src/shared/style.js
+++ src/shared/style.js
@@ -15,4 +15,24 @@ export const globleStyles = StyleSheet.create({
     color: "#707070",
     fontSize: 14,
   },
+
+  buttonPrimary: {
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 24,
+    height: 40,
+    width: 117,
+  },
+
+  buttonOutLine: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 24,
+    borderWidth: 1.5,
+    borderColor: "#000",
+    height: 40,
+    width: 117,
+  },
 });
