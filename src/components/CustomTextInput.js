7cd4f3d52f7f3a7846d2de674d17400c72608d20
Avishka Shyaman <avishkas@akvasoft.com>
2022-11-30 15:30:12 +0530

    changed textInput to accept mutiple text lines

--- src/components/CustomTextInput.js
+++ src/components/CustomTextInput.js
@@ -1,17 +1,32 @@
 import { TextInput, Text, View } from "react-native";
 import { globleStyles } from "../shared/style";
 
-function CustomTextInput({ label, value, onChangeText, marginBottom = 16 }) {
+function CustomTextInput({
+  label,
+  value,
+  onChangeText,
+  marginBottom = 16,
+  multiline = false,
+  numberOfLines = 0
+}) {
   return (
-    <View style={{marginBottom,}}>
-      <Text style={[globleStyles.body, globleStyles.inputLable]}>
-        {label}
-      </Text>
-      <TextInput
-        style={globleStyles.input}
-        onChangeText={onChangeText}
-        value={value}
-      />
+    <View style={{ marginBottom }}>
+      <Text style={[globleStyles.body, globleStyles.inputLable]}>{label}</Text>
+      {multiline ? (
+        <TextInput
+          style={[globleStyles.input, {height: "auto", textAlignVertical: 'top'}]}
+          onChangeText={onChangeText}
+          value={value}
+          multiline
+          numberOfLines={numberOfLines}
+        />
+      ) : (
+        <TextInput
+          style={globleStyles.input}
+          onChangeText={onChangeText}
+          value={value}
+        />
+      )}
     </View>
   );
 }
